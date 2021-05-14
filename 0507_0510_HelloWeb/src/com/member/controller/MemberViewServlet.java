9d9729ad8c278e3945c29dbfaba51b3464cb7c5f
Jang <zn2309@naver.com>
2021-05-14 20:24:52 +0900

    양방향 암호화 - 폰 / 이메일 저장 로직에 적용 (signup / update) memberView 들어갈때 서블릿 통해서 들어가게 변경(주소창에 .jsp 안나오게)

--- 0507_0510_HelloWeb/src/com/member/controller/MemberViewServlet.java
+++ 0507_0510_HelloWeb/src/com/member/controller/MemberViewServlet.java
@@ -1,6 +1,7 @@
 package com.member.controller;
 
 import java.io.*;
+import static com.common.AESEncrypt.*;
 
 import javax.servlet.*;
 import javax.servlet.annotation.*;
@@ -34,6 +35,21 @@ public class MemberViewServlet extends HttpServlet {
 		//세션에서 가져와도 되는데 프론트에서 쿼리스트링으로 가져오는걸로 연습해라..
 		String userId = request.getParameter("userId");
 		Member m = ms.selectMemberId(userId);
+		
+		
+		try {
+			m.setEmail(decrypt(m.getEmail()));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			m.setPhone(decrypt(m.getPhone()));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		//각각 예외처리 하는 이유는 에러발생했을때 어디서 난건지 확인하기 위함
+		
 		request.setAttribute("Member", m);
 		request.getRequestDispatcher("/views/memberView.jsp").forward(request, response);
 		
--- 0507_0510_HelloWeb/src/com/member/controller/SignUpServlet.java
+++ 0507_0510_HelloWeb/src/com/member/controller/SignUpServlet.java
@@ -1,13 +1,12 @@
 package com.member.controller;
 
-import java.io.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import javax.servlet.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.*;
-
-import com.member.model.service.*;
-import com.member.model.vo.*;
 /**
  * Servlet implementation class SignUpServlet
  */
@@ -18,7 +17,6 @@ public class SignUpServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-	MemberService ms = new MemberService();
     public SignUpServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -28,43 +26,7 @@ public class SignUpServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Member m = new Member();
-//		request.setCharacterEncoding("utf-8");
-		m.setUserId(request.getParameter("userId"));
-		m.setPassword(request.getParameter("password"));
-		m.setUserName(request.getParameter("userName"));
-		m.setAge(Integer.parseInt(request.getParameter("age")));
-		m.setEmail(request.getParameter("email"));
-		m.setPhone(request.getParameter("phone"));
-		m.setAddress(request.getParameter("address"));
-		m.setGender(request.getParameter("gender"));
-		m.setHobby(String.join(",",request.getParameterValues("hobby"))); //hobby는 String[] getParmeterValues()로 받아야됨
-		
-		int result = ms.memberSignup(m);
-		String msg="";
-		String location = "/";
-		if(result!=0) {
-			msg = "가입에 성공했습니다.";
-		}else {
-			msg = "가입에 실패했습니다.";
-			location="/views/signup.jsp";
-		}
-		request.setAttribute("msg", msg);
-		request.setAttribute("loc", location);
-		request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
-		
-//		Member acceptM = ms.memberSignup(m);
-//		if(acceptM!=null) {
-//			request.setAttribute("userId", acceptM.getUserId());
-//			request.setAttribute("password", acceptM.getPassword());
-//			request.getRequestDispatcher("/login").forward(request, response);
-//		}else {
-//			String msg = "가입에 실패했습니다.";
-//			String location = "/";
-//			request.setAttribute("msg", msg);
-//			request.setAttribute("loc", location);
-//			request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
-//		}
+		request.getRequestDispatcher("/views/signup.jsp").forward(request, response);
 	}
 
 	/**
