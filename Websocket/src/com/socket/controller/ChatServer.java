fa49c3c370be602a3643293b5945f51c8ee664ef
Jang <zn2309@naver.com>
2021-05-25 21:25:11 +0900

    web socket

--- Websocket/src/com/socket/controller/ChatServer.java
+++ Websocket/src/com/socket/controller/ChatServer.java
@@ -1,5 +1,7 @@
 package com.socket.controller;
 
+import java.util.*;
+
 import javax.websocket.*;
 import javax.websocket.server.*;
 
@@ -12,7 +14,21 @@ public class ChatServer {
 	}
 	@OnMessage
 	public void message(Session session, String msg) {
+		//접속한 모든 세션 가져오기 -> session.getOpenSessions();
+		// List Set Map 등으로 관리함 -> 이후에 Spring 가서 또 쓸거니까 잘 해야됨
+		Set<Session> clients = session.getOpenSessions();
 		
+		//client 정보 저장 : session.getUserProperties()불러와서 .put() 해준다.
+		session.getUserProperties().put("msg", clients);
+		
+		
+		for(Session s : clients) {
+			try {
+			s.getBasicRemote().sendText(msg); //stream 처리라서 Exception 처리 필요
+			}catch(Exception e) {
+				
+			}
+		}
 	}
 	@OnClose
 	public void close(Session session, CloseReason reason) {
