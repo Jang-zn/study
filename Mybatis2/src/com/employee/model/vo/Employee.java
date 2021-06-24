673b86357dc06209e384027149de17daa2cf7d3f
Jang <zn2309@naver.com>
2021-06-24 19:50:38 +0900

    lombok lib 적용

--- Mybatis2/src/com/employee/model/vo/Employee.java
+++ Mybatis2/src/com/employee/model/vo/Employee.java
@@ -1,7 +1,11 @@
 package com.employee.model.vo;
 
 import java.util.Date;
+
+import lombok.Data;
 	//lombok - vo의 getter / setter / tostring / hashcode / equals를 overriding해주는 library
+	//클래스 선언부에 annotation 작성해줌
+@Data
 public class Employee {
 	private int empId;
 	private String empName;
@@ -17,150 +21,5 @@ public class Employee {
 	private Date hireDate;
 	private Date entDate;
 	private String entYn;
-	
-	public Employee() {
-	
-	}
-
-	public Employee(int empId, String empName, String empNo, String email, String phone, String deptCode,
-			String jobCode, String salLevel, int salary, double bonus, int managerId, Date hireDate, Date entDate,
-			String entYn) {
-		super();
-		this.empId = empId;
-		this.empName = empName;
-		this.empNo = empNo;
-		this.email = email;
-		this.phone = phone;
-		this.deptCode = deptCode;
-		this.jobCode = jobCode;
-		this.salLevel = salLevel;
-		this.salary = salary;
-		this.bonus = bonus;
-		this.managerId = managerId;
-		this.hireDate = hireDate;
-		this.entDate = entDate;
-		this.entYn = entYn;
-	}
-
-	public int getEmpId() {
-		return empId;
-	}
-
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public String getEmpNo() {
-		return empNo;
-	}
-
-	public void setEmpNo(String empNo) {
-		this.empNo = empNo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getDeptCode() {
-		return deptCode;
-	}
-
-	public void setDeptCode(String deptCode) {
-		this.deptCode = deptCode;
-	}
-
-	public String getJobCode() {
-		return jobCode;
-	}
-
-	public void setJobCode(String jobCode) {
-		this.jobCode = jobCode;
-	}
-
-	public String getSalLevel() {
-		return salLevel;
-	}
-
-	public void setSalLevel(String salLevel) {
-		this.salLevel = salLevel;
-	}
-
-	public int getSalary() {
-		return salary;
-	}
-
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-
-	public double getBonus() {
-		return bonus;
-	}
-
-	public void setBonus(double bonus) {
-		this.bonus = bonus;
-	}
-
-	public int getManagerId() {
-		return managerId;
-	}
-
-	public void setManagerId(int managerId) {
-		this.managerId = managerId;
-	}
-
-	public Date getHireDate() {
-		return hireDate;
-	}
-
-	public void setHireDate(Date hireDate) {
-		this.hireDate = hireDate;
-	}
-
-	public Date getEntDate() {
-		return entDate;
-	}
-
-	public void setEntDate(Date entDate) {
-		this.entDate = entDate;
-	}
-
-	public String getEntYn() {
-		return entYn;
-	}
-
-	public void setEntYn(String entYn) {
-		this.entYn = entYn;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empNo=" + empNo + ", email=" + email
-				+ ", phone=" + phone + ", deptCode=" + deptCode + ", jobCode=" + jobCode + ", salLevel=" + salLevel
-				+ ", salary=" + salary + ", bonus=" + bonus + ", managerId=" + managerId + ", hireDate=" + hireDate
-				+ ", entDate=" + entDate + ", entYn=" + entYn + "]";
-	}
-
-	
+		
 }
