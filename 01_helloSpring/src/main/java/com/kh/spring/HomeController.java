32ef06efd9c7eb1e1bb6fce04f17bf9f064130d9
Jang <zn2309@naver.com>
2021-06-30 18:20:36 +0900

    bean 등록

--- 01_helloSpring/src/main/java/com/kh/spring/HomeController.java
+++ 01_helloSpring/src/main/java/com/kh/spring/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	// bean 을 변수에 대입하라는 annotation : @Autowired
+	@Autowired
+	private Fruit f;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -26,6 +31,10 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		
+		System.out.println(f);
+		
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -33,7 +42,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "home"; //default : requestDispatcher.forward
 	}
 	
 }
