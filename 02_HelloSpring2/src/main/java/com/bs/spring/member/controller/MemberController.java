5f13001a09282e7cc5583c39854fbc5f44008fc2
Jang <zn2309@naver.com>
2021-07-05 20:42:39 +0900

    단방향 암호화 dependency 적용

--- 02_HelloSpring2/src/main/java/com/bs/spring/member/controller/MemberController.java
+++ 02_HelloSpring2/src/main/java/com/bs/spring/member/controller/MemberController.java
@@ -10,12 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bs.spring.member.model.service.MemberService;
 import com.bs.spring.member.model.vo.Member;
 
 @Controller
-@SessionAttributes("login")
+@SessionAttributes({"login"})
 public class MemberController {
 	@Autowired
 	MemberService ms;
@@ -26,6 +27,8 @@ public class MemberController {
 		//session.setAttribute("login", member);
 		//model의 attribute를 session scope로 이동 가능 (annotation 이용 @SessionAttributes)
 		// 클래스 선언부에 선언해주면 된다.. @SessionAttributes("model의 key값")
+		//@SessionAttributes({"model의 key값","key","key",...}) 배열로도 넣을수 있음
+		//Session에 Attribute가 넘어가는 순간은 viewResolver 도달시점이라서 여기서 syso 찍어도 안나옴
 		m.addAttribute("login",member);
 		return "index";
 	}
@@ -35,9 +38,14 @@ public class MemberController {
 	}
 	
 	@RequestMapping("/member/logout.do")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "index";
+	public String logout(HttpSession session, SessionStatus ss) {
+		//@SessionAttributes로 넘겨준 Attribute는 invalidate로 못없앰
+		//SessionStatus 객체를 완료시켜줘서 없애줘야됨 (setComplete())
+		if(!ss.isComplete()) {
+			ss.setComplete();
+		}
+		//index 보내면 주소가 남아있으니까 redirect 해준다
+		return "redirect:/";//servlet 주소를 줘야됨 지금은 logout이니까 그냥 /
 	}
 	
 	@RequestMapping("/member/insertMember.do")
@@ -46,6 +54,9 @@ public class MemberController {
 		m.addAttribute("loc","/");
 		m.addAttribute("msg",result>0?"가입성공":"가입실패");
 		return "common/msg";
+		//권한인증 / 암호화 해야되는데 이거를 SpringSecurity로 구현 가능
+		
+		
 	}
 	
 }
